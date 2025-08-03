985d7cdc6d6fb497ca5b1779d178116d77477ee6
peteqian <peter.qian.dev@gmail.com>
2025-08-03 16:53:14 +0200

    update

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,6 @@ return {
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    vim.cmd("colorscheme github_dark")
+    vim.cmd("colorscheme github_dark_high_contrast")
   end,
 }
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,13 +1,4 @@
 return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      picker = {
-        hidden = true, -- for hidden files
-        ignored = true, -- for .gitignore files
-      },
-    },
-  },
   -- buffer line
   {
     "akinsho/bufferline.nvim",
@@ -25,44 +16,23 @@ return {
       },
     },
   },
-  -- filename
-  {
-    "b0o/incline.nvim",
-    event = "BufReadPre",
-    config = function()
-      require("incline").setup({
-        window = { margin = { vertical = 0, horizontal = 1 } },
-        hide = {
-          cursorline = true,
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
         },
-        render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if vim.bo[props.buf].modified then
-            filename = "[+] " .. filename
-          end
-
-          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-          return { { icon, guifg = color }, { " " }, { filename } }
-        end,
-      })
-    end,
-  },
-  -- statusline
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-      local LazyVim = require("lazyvim.util")
-      opts.sections.lualine_c[4] = {
-        LazyVim.lualine.pretty_path({
-          length = 0,
-          relative = "cwd",
-          modified_hl = "MatchParen",
-          directory_hl = "",
-          filename_hl = "Bold",
-          modified_sign = "",
-          readonly_icon = " 󰌾 ",
-        }),
-      }
-    end,
+        files = {
+          hidden = false,
+          ignored = false,
+        },
+        grep = {
+          hidden = false,
+          ignored = false,
+        },
+      },
+    },
   },
 }
