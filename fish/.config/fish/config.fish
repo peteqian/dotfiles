c28e661296a03a1f349315585a1bad46cc263193
Peter Qian <peter.qian@sciflow.net>
2026-01-23 15:53:18 +0100

    (fish): update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,6 +18,7 @@ alias g git
 command -qv nvim && alias vim nvim
 
 set -gx EDITOR nvim
+set -gx VISUAL code
 
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
@@ -33,6 +34,32 @@ set -gx PATH $GOPATH/bin $PATH
 # Bun
 set -g BUNPATH $HOME/.bun
 set -gx PATH $BUNPATH/bin $PATH
+
+# Deno
+set -gx PATH $HOME/.deno/bin $PATH
+
+# pnpm
+set -gx PNPM_HOME /Users/applecakes/Library/pnpm
+set -gx PATH $PNPM_HOME $PATH
+
+# Homebrew
+set -gx PATH /opt/homebrew/bin $PATH
+
+# veraPDF
+set -gx PATH /Users/applecakes/verapdf $PATH
+
+# opencode
+set -gx PATH /Users/applecakes/.opencode/bin $PATH
+
+# PostgreSQL
+set -gx PATH /opt/homebrew/opt/postgresql@17/bin $PATH
+
+# # NVM
+# set -gx NVM_DIR $HOME/.nvm
+
+# bass source $NVM_DIR/nvm.sh
+# Fish
+# load_nvm >/dev/stderr
 
 switch (uname)
     case Darwin
@@ -80,5 +107,119 @@ if test -f $LOCAL_CONFIG
     source $LOCAL_CONFIG
 end
 
-oh-my-posh init fish --config ~/.config/omp/takppuccin.omp.json | source
+# oh-my-posh init fish --config ~/.config/fish/omp/catppuccin-custom.json | source
+oh-my-posh init fish --config ~/.config/fish/omp/takppuccin.omp.json | source
+# starship init fish | source
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/applecakes/.cache/lm-studio/bin
+set -x PATH $HOME/.pyenv/bin $PATH
+status --is-interactive; and source (pyenv init -|psub)
+
 zoxide init fish | source
+test -f /opt/homebrew/share/autojump/autojump.fish; and source /opt/homebrew/share/autojump/autojump.fish
+# =============================================================================
+#
+# Utility functions for zoxide.
+#
+
+# pwd based on the value of _ZO_RESOLVE_SYMLINKS.
+function __zoxide_pwd
+    builtin pwd -L
+end
+
+# A copy of fish's internal cd function. This makes it possible to use
+# `alias cd=z` without causing an infinite loop.
+if ! builtin functions --query __zoxide_cd_internal
+    string replace --regex -- '^function cd\s' 'function __zoxide_cd_internal ' <$__fish_data_dir/functions/cd.fish | source
+end
+
+# cd + custom logic based on the value of _ZO_ECHO.
+function __zoxide_cd
+    if set -q __zoxide_loop
+        builtin echo "zoxide: infinite loop detected"
+        builtin echo "Avoid aliasing `cd` to `z` directly, use `zoxide init --cmd=cd fish` instead"
+        return 1
+    end
+    __zoxide_loop=1 __zoxide_cd_internal $argv
+end
+
+# =============================================================================
+#
+# Hook configuration for zoxide.
+#
+
+# Initialize hook to add new entries to the database.
+function __zoxide_hook --on-variable PWD
+    test -z "$fish_private_mode"
+    and command zoxide add -- (__zoxide_pwd)
+end
+
+# =============================================================================
+#
+# When using zoxide with --no-cmd, alias these internal functions as desired.
+#
+
+# Jump to a directory using only keywords.
+function __zoxide_z
+    set -l argc (builtin count $argv)
+    if test $argc -eq 0
+        __zoxide_cd $HOME
+    else if test "$argv" = -
+        __zoxide_cd -
+    else if test $argc -eq 1 -a -d $argv[1]
+        __zoxide_cd $argv[1]
+    else if test $argc -eq 2 -a $argv[1] = --
+        __zoxide_cd -- $argv[2]
+    else
+        set -l result (command zoxide query --exclude (__zoxide_pwd) -- $argv)
+        and __zoxide_cd $result
+    end
+end
+
+# Completions.
+function __zoxide_z_complete
+    set -l tokens (builtin commandline --current-process --tokenize)
+    set -l curr_tokens (builtin commandline --cut-at-cursor --current-process --tokenize)
+
+    if test (builtin count $tokens) -le 2 -a (builtin count $curr_tokens) -eq 1
+        # If there are < 2 arguments, use `cd` completions.
+        complete --do-complete "'' "(builtin commandline --cut-at-cursor --current-token) | string match --regex -- '.*/$'
+    else if test (builtin count $tokens) -eq (builtin count $curr_tokens)
+        # If the last argument is empty, use interactive selection.
+        set -l query $tokens[2..-1]
+        set -l result (command zoxide query --exclude (__zoxide_pwd) --interactive -- $query)
+        and __zoxide_cd $result
+        and builtin commandline --function cancel-commandline repaint
+    end
+end
+complete --command __zoxide_z --no-files --arguments '(__zoxide_z_complete)'
+
+# Jump to a directory using interactive search.
+function __zoxide_zi
+    set -l result (command zoxide query --interactive -- $argv)
+    and __zoxide_cd $result
+end
+
+# =============================================================================
+#
+# Commands for zoxide. Disable these using --no-cmd.
+#
+
+abbr --erase cd &>/dev/null
+alias cd=__zoxide_z
+
+abbr --erase cdi &>/dev/null
+alias cdi=__zoxide_zi
+
+# =============================================================================
+#
+# To initialize zoxide, add this to your configuration (usually
+# ~/.config/fish/config.fish):
+#
+#   zoxide init fish | source
+fish_add_path $HOME/.local/bin
+alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+
+/Users/applecakes/.local/bin/mise activate fish | source # added by https://mise.run/fish
+mise activate fish --shims | source
